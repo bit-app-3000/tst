b34267ad3d71a1fc36d3df33cc4b9368dbff3eb8
bt <b.tykhyy@mmdsmart.com>
2021-01-21 15:08:08 +0200

    wip

--- init.sh
+++ init.sh
@@ -8,6 +8,12 @@ CLUSTER=https://kubernetes.default.svc
 SRC=https://github.com/argoproj/argocd-example-apps.git
 
 
+
+kubectl apply -f gitops/project.yml
+kubectl apply -f gitops/main.yml
+
+argocd app get main
+argocd app sync main
 #argocd login 127.0.0.1 --username=admin --password=111111 --insecure
 #argocd proj create ${PROJECT} -d ${CLUSTER},${NS} -s ${SRC}
 
