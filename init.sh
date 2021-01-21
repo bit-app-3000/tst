4db3ac058367d6e22454b99f68703bb509b48fb4
bt <b.tykhyy@mmdsmart.com>
2021-01-21 15:43:22 +0200

    wip

--- init.sh
+++ init.sh
@@ -29,13 +29,17 @@ cd gitops
 #
 #step certificate inspect sample-trust.crt
 
-kubectl -n linkerd create secret tls linkerd-trust-anchor \
-  --cert sample-trust.crt \
-  --key sample-trust.key \
-  --dry-run=client -oyaml | \
-kubeseal --controller-name=sealed-secrets -oyaml - | \
-kubectl patch -f - \
-  -p '{"spec": {"template": {"type":"kubernetes.io/tls", "metadata": {"labels": {"linkerd.io/control-plane-component":"identity", "linkerd.io/control-plane-ns":"linkerd"}, "annotations": {"linkerd.io/created-by":"linkerd/cli stable-2.8.1", "linkerd.io/identity-issuer-expiry":"2021-07-19T20:51:01Z"}}}}}' \
-  --dry-run=client \
-  --type=merge \
-  --local -oyaml > resources/linkerd/trust-anchor.yaml
+#kubectl -n linkerd create secret tls linkerd-trust-anchor \
+#  --cert sample-trust.crt \
+#  --key sample-trust.key \
+#  --dry-run=client -oyaml | \
+#kubeseal --controller-name=sealed-secrets -oyaml - | \
+#kubectl patch -f - \
+#  -p '{"spec": {"template": {"type":"kubernetes.io/tls", "metadata": {"labels": {"linkerd.io/control-plane-component":"identity", "linkerd.io/control-plane-ns":"linkerd"}, "annotations": {"linkerd.io/created-by":"linkerd/cli stable-2.8.1", "linkerd.io/identity-issuer-expiry":"2021-07-19T20:51:01Z"}}}}}' \
+#  --dry-run=client \
+#  --type=merge \
+#  --local -oyaml > resources/linkerd/trust-anchor.yaml
+
+
+argocd app get linkerd -ojson | \
+  jq -r '.spec.source.helm.parameters[] | select(.name == "global.identityTrustAnchorsPEM") | .value'
