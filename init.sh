2aa5c59cb1bb70c8b3a9729d0edd765ca05e0930
bt <b.tykhyy@mmdsmart.com>
2021-01-21 15:18:54 +0200

    wip

--- init.sh
+++ init.sh
@@ -10,10 +10,10 @@ SRC=https://github.com/argoproj/argocd-example-apps.git
 
 
 kubectl apply -f gitops/project.yml
-kubectl apply -f gitops/main.yml
+kubectl apply -f gitops/maap.yml
 
-argocd app get main
-argocd app sync main
+#argocd app get maap
+argocd app sync maap
 #argocd login 127.0.0.1 --username=admin --password=111111 --insecure
 #argocd proj create ${PROJECT} -d ${CLUSTER},${NS} -s ${SRC}
 
